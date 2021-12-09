da62658af86f4c9758ec2283ceb2888e03397be0
sachinpr0001 <sachinpr0001@gmail.com>
2021-12-09 16:35:17 +0545

    basic to-do list wih JS

--- demo01/js-basics/todolist.js
+++ demo01/js-basics/todolist.js
@@ -1,1 +1,7 @@
 let addBtn = document.getElementById('addBtn')
+let newTask = document.getElementById('newTask')
+let taskList = document.getElementById('taskList')
+
+addBtn.onclick = function () {
+    taskList.innerHTML += '<li>' + newTask.value + '</li>' 
+}
